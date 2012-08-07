8d4bdb8f1142a52e4f08441a92083d03c85f8690
Igor Oliveira <igor.o@sisa.samsung.com>
2012-08-07 11:22:07 -0700

    src - add gpuprocess dispatch. It would be used to call the system OpenGL(ES)/GLX/EGL functions. Currently we just support OpenGL and GLX.

---
+++ src/gpuprocess_dispatch.c
@@ -0,0 +1,52 @@
+#include <dlfcn.h>
+
+#include "gpuprocess_dispatch.h"
+
+#define LIBNAME "libGL.so"
+
+typedef enum gpuprocess_sym_load_type {
+    DLSYM,
+    GLXGETPROC
+} gpuprocess_sym_load_type_t;
+
+static void
+gpuprocess_dispatch_init_entries(gpuprocess_dispatch_t *dispatch,
+				 gpuprocess_dispatch_entry_t *entries,
+				 gpuprocess_sym_load_type_t type)
+{
+   void *handle = dlopen (LIBNAME, RTLD_LAZY);
+   gpuprocess_dispatch_entry_t *entry = entries;
+   char *error;
+
+   while (entry->name[GPUPROCESS_GL_DISPATCH_NAME_CORE] != NULL) {
+      int i;
+      GenericFunc func = 0;
+      void *dispatch_ptr = &((char *) dispatch)[entry->offset];
+
+      for (i = 0; i < GPUPROCESS_GL_DISPATCH_NAME_COUNT; i++) {
+          const char *name = entry->name[i];
+          if (type == DLSYM)
+              func = dlsym(handle, name);
+          else {
+	      func = dispatch->GetProcAddress(name);
+              if (!func)
+                  func = dlsym(handle, name);
+          }
+
+	  if (func)
+	      break;
+      }
+
+      *((GenericFunc *) dispatch_ptr) = func;
+
+      ++entry;
+   }
+}
+
+void
+gpuprocess_dispatch_init(gpuprocess_dispatch_t *dispatch)
+{
+    // We should always load dispatch_glx_entries first
+    gpuprocess_dispatch_init_entries(dispatch, dispatch_glx_entries, DLSYM);
+    gpuprocess_dispatch_init_entries(dispatch, dispatch_opengl_entries, GLXGETPROC);
+}
