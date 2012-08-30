54ad3e19e96069c1457abba869e4ff3cd9f3efd6
Martin Robinson <mrobinson@igalia.com>
2012-08-30 16:01:55 -0700

    Begin to autogenerate some client sources
    Crudely repurpose the Chromium command-buffer code generator to start
    generating client-side API. This will improve with time, but this serves
    as a hacky starting point.

--- src/gpuprocess_gles2_api.c
+++ src/gpuprocess_gles2_api.c
@@ -1,39 +1,4 @@
-#include <GLES/gl.h>
-#include <GLES/glext.h>
-#include <GLES2/gl2.h>
-#include <GLES2/gl2ext.h>
-#include <string.h>
-#include <stdlib.h>
-
-#include "gpuprocess_types_private.h"
-#include "gpuprocess_thread_private.h"
-#include "gpuprocess_egl_states.h"
-
-extern __thread v_link_list_t *active_state;
-extern __thread gpu_thread *srv_thread;
-extern __thread int unpack_alignment;
-
-static inline v_bool_t
-_is_error_state (void )
-{
-    egl_state_t *state;
-
-    if (! active_state || ! srv_thread)
-        return TRUE;
-
-    state = (egl_state_t *) active_state->data;
-
-    if (! state ||
-        ! (state->display == EGL_NO_DISPLAY ||
-           state->context == EGL_NO_CONTEXT) ||
-	state->active == FALSE) {
-        return TRUE;
-    }
-
-    return FALSE;
-}
-
-#include "gpuprocess_gles2_api_autogen.c"
+#include "gpuprocess_gles2_api_private.h"
 
 void glBufferData (GLenum target, GLsizeiptr size,
                    const GLvoid *data, GLenum usage)
