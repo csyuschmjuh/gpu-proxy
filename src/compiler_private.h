acad11fe4dcd0b110029fc055e64ad0b85ea57de
Martin Robinson <mrobinson@igalia.com>
2012-09-11 12:19:18 -0700

    Always enable the tests
    Instead of overriding the meaning of static, allow explicitly exposing
    methods to tests with the "exposed_to_tests" define. When this is used,
    only while compiling tests will the methods be non-static. Methods
    exposed to tests must be placed in the *_tests.h headers which are
    included from test files.

--- src/compiler_private.h
+++ src/compiler_private.h
@@ -6,6 +6,12 @@
 #define public  __attribute__((visibility("default")))
 #define private __attribute__((visibility("hidden")))
 
+#ifndef ENABLE_TESTING
+#define exposed_to_tests static
+#else
+#define exposed_to_tests
+#endif
+
 #if defined(__GNUC__) && (__GNUC__ > 2) && defined(__OPTIMIZE__)
 #define likely(expr) (__builtin_expect (!!(expr), 1))
 #define unlikely(expr) (__builtin_expect (!!(expr), 0))
