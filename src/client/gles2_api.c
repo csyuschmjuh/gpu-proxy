77f62dc6cf3e50fd7a7527308093c83450f51348
Martin Robinson <mrobinson@igalia.com>
2012-10-02 11:31:09 -0700

    Fix a compilation error

--- src/client/gles2_api.c
+++ src/client/gles2_api.c
@@ -1387,8 +1387,9 @@ glUniformMatrix2fv (GLint location,
         return;
 
     if (count <= 0 || value == NULL)
+        return;
     /* post command and no wait */
-    
+
     copy = (GLfloat *) malloc (sizeof (GLfloat) * 16 * count);
     memcpy ((void *)copy, (void *)value, sizeof (GLfloat) * 16 * count);
     /* XXX: post command and no wait */
@@ -1404,10 +1405,12 @@ glUniformMatrix3fv (GLint location,
 
     if (_is_error_state ())
         return;
-    
+
     if (count <= 0 || value == NULL)
+        return;
+
     /* post command and no wait */
-    
+
     copy = (GLfloat *) malloc (sizeof (GLfloat) * 9 * count);
     memcpy ((void *)copy, (void *)value, sizeof (GLfloat) * 9 * count);
     /* XXX: post command and no wait */
@@ -1423,10 +1426,11 @@ glUniformMatrix4fv (GLint location,
 
     if (_is_error_state ())
         return;
-    
+
     if (count <= 0 || value == NULL)
+        return;
     /* post command and no wait */
-    
+
     copy = (GLfloat *) malloc (sizeof (GLfloat) * 4 * count);
     memcpy ((void *)copy, (void *)value, sizeof (GLfloat) * 4 * count);
     /* XXX: post command and no wait */
