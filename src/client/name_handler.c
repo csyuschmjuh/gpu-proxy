fa525d719138bc09c27fd2b792e1e24e96d9acd6
Alejandro G. Castro <alex@igalia.com>
2012-11-15 18:50:35 +0100

    Make name_handler static.
    This way we make sure we can allow sharing it when contexts are
    shared.

--- src/client/name_handler.c
+++ src/client/name_handler.c
@@ -1,51 +1,66 @@
 #include "name_handler.h"
 #include <limits.h>
 
+typedef struct name_handler {
+    unsigned int last_name;
+    bool initialized;
+    HashTable *used_names;
+} name_handler_t;
+
 static char *dummyData = "";
+static name_handler_t name_handler;
 
-name_handler_t *
-name_handler_create ()
+mutex_static_init (name_handler_mutex);
+
+void
+name_handler_init ()
 {
-    name_handler_t *name_handler = (name_handler_t *)malloc (sizeof (name_handler_t));
-    name_handler->last_name = 0;
-    name_handler->used_names = NewHashTable(free);
-    return name_handler;
+    if (name_handler.initialized)
+        return;
+
+    mutex_lock (name_handler_mutex);
+
+    name_handler.initialized = true;
+    name_handler.last_name = 0;
+    name_handler.used_names = NewHashTable(free);
+
+    mutex_unlock (name_handler_mutex);
 }
 
 void
-name_handler_destroy (name_handler_t *name_handler)
-{
-
-    DeleteHashTable (name_handler->used_names);
-    free (name_handler);
-}
-
-void
-name_handler_alloc_names (name_handler_t *name_handler,
-                          GLsizei n,
+name_handler_alloc_names (GLsizei n,
                           GLuint *buffers)
 {
-   int i = 0;
+    int i = 0;
+
+    mutex_lock (name_handler_mutex);
+
     while (i < n) {
-        int next_name = ++name_handler->last_name;
+        int next_name = ++name_handler.last_name;
 
         if (next_name == UINT_MAX)
-            name_handler->last_name = next_name = 0;
+            name_handler.last_name = next_name = 0;
 
-        if (!HashLookup (name_handler->used_names, next_name)) {
+        if (!HashLookup (name_handler.used_names, next_name)) {
             buffers[i] = next_name;
-            HashInsert (name_handler->used_names, next_name, dummyData);
+            HashInsert (name_handler.used_names, next_name, dummyData);
             i++;
         }
     }
+
+    mutex_unlock (name_handler_mutex);
 }
 
 void
-name_handler_delete_names (name_handler_t *name_handler,
-                           GLsizei n,
+name_handler_delete_names (GLsizei n,
                            const GLuint *buffers)
 {
     int i;
+
+    mutex_lock (name_handler_mutex);
+
     for (i = 0; i < n; i++)
-        HashRemove (name_handler->used_names, buffers[i]);
+        HashRemove (name_handler.used_names, buffers[i]);
+
+    mutex_unlock (name_handler_mutex);
 }
