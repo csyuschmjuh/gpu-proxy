805331f63ecf977a3d605d5c432ac663aad7b0d9
Martin Robinson <mrobinson@igalia.com>
2012-11-27 11:53:29 -0800

    Reuse old name_handler names more readily
    This fixes a memory leak due to the fact that the hash table
    implementation does no rehashing. Thus if we continue to add names with
    larger and larger value, the hash table will continue to grow in size.
    
    We work around this issue by simply saving a list of freed names and
    not using hash table entirely. This should both speed up the operation
    of the name handler and use less memory.

--- src/client/name_handler.c
+++ src/client/name_handler.c
@@ -4,10 +4,8 @@
 typedef struct name_handler {
     unsigned int last_name;
     bool initialized;
-    HashTable *used_names;
+    link_list_t *reusable_names;
 } name_handler_t;
-
-static char *dummyData = "";
 static name_handler_t name_handler;
 
 mutex_static_init (name_handler_mutex);
@@ -19,10 +17,11 @@ name_handler_init ()
         return;
 
     mutex_lock (name_handler_mutex);
+    if (name_handler.initialized)
+        return;
 
-    name_handler.initialized = true;
+    name_handler.reusable_names = NULL;
     name_handler.last_name = 0;
-    name_handler.used_names = NewHashTable(NULL);
 
     mutex_unlock (name_handler_mutex);
 }
@@ -31,21 +30,21 @@ void
 name_handler_alloc_names (GLsizei n,
                           GLuint *buffers)
 {
-    int i = 0;
-
     mutex_lock (name_handler_mutex);
 
+    int i = 0;
     while (i < n) {
-        int next_name = ++name_handler.last_name;
-
-        if (next_name == UINT_MAX)
-            name_handler.last_name = next_name = 0;
-
-        if (!HashLookup (name_handler.used_names, next_name)) {
-            buffers[i] = next_name;
-            HashInsert (name_handler.used_names, next_name, dummyData);
-            i++;
+        unsigned next_name = 0;
+        if (name_handler.reusable_names != NULL) {
+            next_name = * ((GLuint *) name_handler.reusable_names->data);
+            link_list_delete_element (&name_handler.reusable_names, name_handler.reusable_names);
+        } else {
+            next_name = ++name_handler.last_name;
+            assert (next_name != UINT_MAX);
         }
+
+        buffers[i] = next_name;
+        i++;
     }
 
     mutex_unlock (name_handler_mutex);
@@ -55,12 +54,14 @@ void
 name_handler_delete_names (GLsizei n,
                            const GLuint *buffers)
 {
-    int i;
-
     mutex_lock (name_handler_mutex);
 
-    for (i = 0; i < n; i++)
-        HashRemove (name_handler.used_names, buffers[i]);
+    int i;
+    for (i = 0; i < n; i++) {
+        GLuint *reusable_name = malloc (sizeof (GLuint));
+        *reusable_name = buffers[i];
+        link_list_append (&name_handler.reusable_names, reusable_name, free);
+    }
 
     mutex_unlock (name_handler_mutex);
 }
